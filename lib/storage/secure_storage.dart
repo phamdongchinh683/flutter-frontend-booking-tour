4e4712c75da94abf4e52c959a104195890b3fd83
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-10 18:19:04 +0700

    update folder project mvc 10-12-2024

---
+++ lib/storage/secure_storage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  Future<void> saveToken(String token) async {
+    await _storage.write(key: 'token', value: token);
+  }
+
+  Future<String?> retrieveToken() async {
+    final token = await _storage.read(key: 'token');
+    return token;
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'token');
+  }
+}
